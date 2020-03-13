c36ff95a290b258991640f451ff3ade27385cb4d
Marius Kohmann <marius.kohmann@gmail.com>
2020-03-13 14:50:20 +0100

    clean sample

--- src/main/kotlin/io/github/mkohm/SomeClass.kt
+++
@@ -1,6 +0,0 @@
-package io.github.mkohm
-
-open class AnotherClass
-
-// This will create a detekt-hint warning
-class SomeClass: AnotherClass()
---
+++ src/main/kotlin/io/github/mkohm/demo/UseCompositionInsteadOfInheritance/SimpleFalsePositive.kt
@@ -0,0 +1,10 @@
+package io.github.mkohm.demo.UseCompositionInsteadOfInheritance
+
+open class Animal {
+    fun eat() {}
+    fun sleep(){}
+}
+
+class Lion : Animal() {
+
+}
